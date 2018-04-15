96b4e9e0f1145323d8c01a9da1b661f6d9c401a1
yonechen <yorkchan94@gmail.com>
2018-04-15 23:18:44 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader"
+            }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
+            }
+          ]
+        }
+        ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ],
+    devServer: {
+      https: true,
+      open: true,
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+      port: 8005,
+      inline: true
+    }
+  }
